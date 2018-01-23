26d0f6c796e32bc57a7624e148be70a3fdd24495
Andrés Gasson <gas@ateasystems.com>
2018-01-23 18:33:52 +1300

    first working version of atea literate - added clj, yaml, cider, lisp, paredit etc

--- init.el
+++ init.el
@@ -5,6 +5,15 @@
 ;; or tangling and loading a literate org configuration file.
 
 ;;; Code:
+;; menu shit remove
+
+(mapc
+ (lambda (mode)
+   (when (fboundp mode)
+     (funcall mode -1)))
+ '(menu-bar-mode tool-bar-mode scroll-bar-mode))
+
+
 
 ;; Don't attempt to find/apply special file handlers to files loaded during startup.
 (let ((file-name-handler-alist nil))
@@ -16,3 +25,17 @@
     (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (toc-org org-plus-contrib magit counsel-projectile ivy evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
